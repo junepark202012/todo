b6421052704e1fbf863c5496fb432300afc6fca4
June Park <junepark202012@gmail.com>
2022-10-29 14:42:04 +0900

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      gray: {
+        100: "#F2F1E9",
+        300: "#D9D7D0",
+        500: "#8C8B87",
+        700: "#595959",
+        900: "#0D0D0D",
+      },
+    },
     extend: {
       fontFamily: {
         sans: [
@@ -16,6 +25,7 @@ module.exports = {
           ...defaultTheme.fontFamily.sans,
         ],
       },
+      colors: {},
     },
   },
   plugins: [],
