4f699daa47c515ee94359325ad5ca1a9c30599a4
June Park <junepark202012@gmail.com>
2022-10-30 22:40:00 +0900

    Remove ui folder

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import { ReactElement } from "react";
-import Head from "next/head";
-import Navbar from "./ui/Navbar";
+import Navbar from "./Navbar";
 
 export default function Layout({ children }: { children: ReactElement }) {
   return (
